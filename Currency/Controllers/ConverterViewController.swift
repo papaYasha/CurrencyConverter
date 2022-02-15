81c02590e8d7bd24ae15a0af7351b1144118c71e
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-02-15 10:43:06 +0300

    config UI, added custom fonts, created template MVC, added AppIcon and flags of country to assets.

---
+++ Currency/Controllers/ConverterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Currency
+//
+//  Created by Macbook on 15.02.22.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
