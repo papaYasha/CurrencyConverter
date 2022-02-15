6c8db7c3e3663550c6a6ae78aa8a813a4634c3fc
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-02-15 10:04:00 +0300

    init project

---
+++ Currency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Currency
+//
+//  Created by Macbook on 15.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
